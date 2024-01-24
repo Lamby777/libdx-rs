cdb6e56f27387c12c861adc6c7ae629485a2e967
Cherry <cherry@sparklet.org>
2024-01-23 19:26:02 -0500

    2.0.0, reworked `foreach!` + added `foreach_dyn!` macro + made `sleep!`   into a function

--- src/lib.rs
+++ src/lib.rs
@@ -3,30 +3,69 @@
 * - Cherry, 4/27/2023
 */
 
-use std::error::Error;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::error::Error;
 
 pub type Result<T, E = Box<dyn Error>> = core::result::Result<T, E>;
 pub type SyncResult<T, E = Box<dyn Error + Send + Sync>> = Result<T, E>;
 
-#[macro_export]
-macro_rules! sleep {
-    ($len:expr) => {
-        thread::sleep(time::Duration::from_millis($len));
-    };
+pub fn sleep(len: u64) {
+    std::thread::sleep(std::time::Duration::from_millis(len));
 }
 
 pub fn choose_rand<T: Copy, V: AsRef<[T]>>(v: V) -> Option<T> {
-	v.as_ref().choose(&mut thread_rng()).map(|value| *value)
+    v.as_ref().choose(&mut thread_rng()).map(|value| *value)
 }
 
+/// Expands to repeat some code for each item in
+/// a provided list of values.
+///
+/// # Example
+/// ```
+/// # use libdx::foreach;
+/// let mut vec = Vec::new();
+/// foreach!([1, 2, 3] => |x| vec.push(x));
+/// assert_eq!(vec, [1, 2, 3]);
+/// ```
 #[macro_export]
 macro_rules! foreach {
-	($logic:expr, $($arg:expr),+) => {
+	([$($arg:expr),+] => $logic:expr) => {
 		{
-			let logic = {$logic};
+			let mut logic = {$logic};
 			$(logic($arg);)+
+		}
+	};
+}
+
+/// Like `foreach!`, but allows you to "iterate" over
+/// identifiers, running your logic on a `&dyn T` trait
+/// object for each one.
+///
+/// # Example
+/// ```
+/// # use libdx::foreach_dyn;
+/// struct Foo;
+/// struct Bar;
+/// struct Baz;
+///
+/// trait Quacks {
+///     fn quack(&self) { println!("Quack!"); }
+/// }
+///
+/// impl Quacks for Foo {};
+/// impl Quacks for Bar {};
+/// impl Quacks for Baz {};
+///
+/// foreach_dyn!([Foo, Bar, Baz] => |x: &dyn Quacks| x.quack());
+/// ```
+///
+#[macro_export]
+macro_rules! foreach_dyn {
+	([$($arg:ident),+] => $logic:expr) => {
+		{
+			let mut logic = {$logic};
+			$(logic(&$arg);)+
 		}
 	};
 }
@@ -38,7 +77,7 @@ macro_rules! map {
     ($($k:expr => $v:expr),* $(,)?) => {{
         core::convert::From::from([$(($k, $v),)*])
     }};
-	
+
     ($($v:expr),* $(,)?) => {{
         core::convert::From::from([$($v,)*])
     }};
